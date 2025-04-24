e1d0bdfe40ccf1766e85d85e3d9c126552210dfd
Kobe Taylor <ktaylor2@linfield.edu>
2025-04-24 11:13:39 -0700

    Renamed the WeatherController and deleted all the mehods that we did not make and ran it to see if our method still works and if it still compiles

---
+++ TDDAPiQuiz/Controllers/TDDController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using TDDAPiQuiz.Data;
+namespace TDDAPiQuiz.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TDDController : ControllerBase
+    {
+        private readonly TDDContext _context;
+
+        public TDDController(TDDContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("ContextTest")]
+        public IActionResult ContextTest()
+        {
+            if (_context != null)
+            {
+                return Ok("Context successful!");
+            }
+            else
+            {
+                return StatusCode(500, "Context not successful");
+            }
+        }
+    }
+}
